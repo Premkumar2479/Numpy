de5f834b321268b2b1e3eeb527bbd32a888b747b
Premkumar2479 <pk2335301@gmail.com>
2026-06-16 11:55:21 +0530

    add code

--- Numpy Array Properties/ndim.py
+++ Numpy Array Properties/ndim.py
@@ -2,3 +2,10 @@
 
 import numpy as np 
 
+arr_1d = np.array([1,2,3,])
+arr_2d = np.array([[1,2,3,],[4,5,6,]])
+arr_3d = np.array([[[1,2],[3,4,],[5,4],[8,9]]])
+
+print(arr_1d.ndim)
+print(arr_2d.ndim)
+print(arr_3d.ndim)
