a5b6d64e29fccff02d8f7d7a18202abc8308fc87
Premkumar2479 <pk2335301@gmail.com>
2026-06-18 15:50:12 +0530

    add to elements rows wise

---
+++ Vectorization/1.py
@@ -0,0 +1,5 @@
+list1 = [1,2,3]
+list2 = [3,4,5]
+
+result = [x+y for x,y in zip(list1,list2)]
+print(result)
---
+++ Vectorization/fast.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+arr1 =np.array([[1,2,3],[1,4,3]])
+arr2 = np.array([[1,2,3],[1,4,3]])
+result = arr1 + arr2
+print(result)
