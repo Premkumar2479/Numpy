91db459682dcd521de290244bb79b39515224ba0
Premkumar2479 <pk2335301@gmail.com>
2026-06-18 11:59:40 +0530

    append add  elements to end

---
+++ Advanced Numpy/append.py
@@ -0,0 +1,7 @@
+#append a new elements to the add in end of the array
+
+import numpy as np
+
+arr = np.array([10,20,30,])
+new_arr = np.append(arr,[40,50,60])
+print(new_arr)
--- Advanced Numpy/insert_in_2d.py
+++ Advanced Numpy/insert_in_2d.py
@@ -3,6 +3,6 @@ import numpy as np
 arr_2d = np.array([[1,2,3],[4,5,6]])
 print(arr_2d)
 
-#insert a new row ar index 1
-new_arr = np.insert(arr_2d,1,[10,20,30,],axis=0)
+#insert a new column at index 1
+new_arr = np.insert(arr_2d,1,[10,20,39],axis=0)
 print(new_arr)
