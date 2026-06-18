0d1cc2ff492d492baa618f5d032667d6f8fccfd5
Premkumar2479 <pk2335301@gmail.com>
2026-06-18 11:47:13 +0530

    insert 1d array

---
+++ Advanced Numpy/insert.py
@@ -0,0 +1,17 @@
+"""
+np.insert(array, index, values,asix=None)
+
+array - original array
+index -
+value -
+axis - 
+axis = 0, row-wise
+1 column wise
+
+"""
+
+import numpy as np
+
+arr = np.array([10,20,30,40,50,60,])
+new_arr = np.insert(arr,2,100)
+print(new_arr)
